57895297361f3078cfed92738aabcfb367a28957
electrobullet <dmitry.pigasin@yandex.ru>
2021-11-04 19:52:28 +0300

    Implement `find_cycle_path`; Split Data class

---
+++ TransportationProblemData.py
@@ -0,0 +1,88 @@
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
+
+from logger import log
+
+
+class TransportationProblemData:
+    """
+    a - обьемы поставщиков;\n
+    b - обьемы потребителей;\n
+    c - матрица затрат на транспортировку;\n
+    r - штраф за излишки/дефицит;\n
+    (все значения в ед. товара)
+    """
+
+    def __init__(
+        self, a: List[int], b: List[int], c: List[List[float]], r: Optional[Dict[str, List[float]]] = None,
+    ) -> None:
+        self.a = a
+        self.b = b
+        self.c = np.array(c)
+        self.r = r if isinstance(r, Dict) else {'a': [0 for _ in range(self.m)], 'b': [0 for _ in range(self.n)]}
+
+    @property
+    def m(self) -> int:
+        """Количество поставщиков (строк матрицы c)."""
+        return len(self.a)
+
+    @property
+    def n(self) -> int:
+        """Количество потребителей (столбцов матрицы c)."""
+        return len(self.b)
+
+    @log('Разница между предложением и спросом: {result}')
+    def get_supply_demand_difference(self) -> int:
+        """Получить разницу между спросом и предложением."""
+        return sum(self.a) - sum(self.b)
+
+    @log('Добавлен фиктивный поставщик с обьемом: {args[1]}')
+    def add_dummy_supplier(self, volume: int) -> None:
+        """Добавить фиктивного поставщика."""
+        e = np.ones((1, self.c.shape[1])) * self.r['b']
+        self.c = np.row_stack((self.c, e))
+        self.a.append(volume)
+
+    @log('Добавлен фиктивный потребитель с обьемом: {args[1]}')
+    def add_dummy_customer(self, volume: int) -> None:
+        """Добавить фиктивного потребителя."""
+        e = np.ones((self.c.shape[0], 1)) * self.r['a']
+        self.c = np.column_stack((self.c, e))
+        self.b.append(volume)
+
+    @log('Целевая функция = {result}')
+    def calculate_cost(self, x: np.ndarray) -> float:
+        """Подсчет стоимости (целевой функции)."""
+        return np.sum(self.c * x)
+
+    @log('Потенциалы: {result}')
+    def calculate_potentials(self, x: np.ndarray) -> Dict[str, np.ndarray]:
+        """Вычисление потенциалов."""
+        res = {'a': np.full(self.m, np.inf), 'b': np.full(self.n, np.inf)}
+        res['a'][0] = 0
+
+        while np.inf in res['a'] or np.inf in res['b']:
+            for i in range(self.m):
+                for j in range(self.n):
+                    if x[i][j] != 0:
+                        if res['a'][i] != np.inf:
+                            res['b'][j] = self.c[i][j] - res['a'][i]
+                        elif res['b'][j] != np.inf:
+                            res['a'][i] = self.c[i][j] - res['b'][j]
+
+        return res
+
+    @log('Оптимальный план: {result}')
+    def is_plan_optimal(self, x: np.ndarray, p: Dict[str, np.ndarray]) -> bool:
+        """Проверка плана на оптимальность."""
+        for i, j in zip(*np.nonzero(x == 0)):
+            if p['a'][i] + p['b'][j] > self.c[i][j]:
+                return False
+
+        return True
+
+    @log('Лучшая свободная клетка для начала цикла пересчета: {result}')
+    def get_best_free_cell(self, x: np.ndarray, p: Dict[str, np.ndarray]) -> Tuple[int, int]:
+        free_cells = tuple(zip(*np.nonzero(x == 0)))
+        return free_cells[np.argmax([p['a'][i] + p['b'][j] - self.c[i][j] for i, j in free_cells])]
---
+++ utils.py
@@ -0,0 +1,192 @@
+from typing import List, Tuple
+
+import numpy as np
+
+from logger import log, logger
+from TransportationProblemData import TransportationProblemData
+
+
+@log('\nНачальный опорный план, полученный методом минимального элемента:\n{result}')
+def get_start_plan_by_min_element_method(data: TransportationProblemData) -> np.ndarray:
+    """Получить начальный опорный план методом минимального элемента."""
+    def get_min_element_position(matrix: np.ndarray) -> Tuple[int, int]:
+        """Получить позицию минимального элемента матрицы."""
+        flat_index = np.argmin(matrix)
+        i = flat_index // data.m
+        j = flat_index - i * data.n
+        return (i, j)
+
+    res = np.zeros((data.m, data.n))
+
+    a = data.a.copy()
+    b = data.b.copy()
+    c = data.c.copy()
+
+    has_dummy_row = False
+    has_dummy_column = False
+
+    if sum(c[-1]) == 0:
+        c[-1] = np.inf
+        has_dummy_row = True
+    elif sum(c[:, -1]) == 0:
+        c[:, -1] = np.inf
+        has_dummy_column = True
+
+    while sum(a) + sum(b) != 0:
+        i, j = get_min_element_position(c)
+        c[i][j] = np.inf
+
+        x = min(a[i], b[j])
+        a[i] -= x
+        b[j] -= x
+
+        res[i][j] = x
+
+        if a[i] == 0:
+            c[i] = np.inf
+
+        if b[j] == 0:
+            c[:, j] = np.inf
+
+        if np.min(c) == np.inf:
+            if has_dummy_row:
+                c[-1] = 0
+            elif has_dummy_column:
+                c[:, -1] = 0
+
+    return res
+
+
+@log('\nНачальный опорный план, найденный методом северо-западного угла:\n{result}')
+def get_start_plan_by_north_west_corner_method(data: TransportationProblemData) -> np.ndarray:
+    """Получить начальный опорный план методом северо-западного угла."""
+    res = np.zeros((data.m, data.n))
+
+    a = data.a.copy()
+    b = data.b.copy()
+    i = 0
+    j = 0
+
+    while i < data.m and j < data.n:
+        x = min(a[i], b[j])
+        a[i] -= x
+        b[j] -= x
+
+        res[i][j] = x
+
+        if a[i] == 0:
+            i += 1
+
+        if b[j] == 0:
+            j += 1
+
+    return res
+
+
+@log('Вырожденный план: {result}')
+def is_degenerate_plan(x: np.ndarray) -> bool:
+    """Проверка плана на вырожденность."""
+    m, n = x.shape
+    return True if np.count_nonzero(x) != m + n - 1 else False
+
+
+@log('Цикл пересчета: {result}')
+def find_cycle_path(x: np.ndarray, start_pos: Tuple[int, int]) -> List[Tuple[int, int]]:
+    def get_posible_moves(bool_table: np.ndarray, path: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+        posible_moves = np.full(bool_table.shape, False)
+
+        current_pos = path[-1]
+        prev_pos = path[-2] if len(path) > 1 else (np.nan, np.nan)
+
+        if current_pos[0] != prev_pos[0]:
+            posible_moves[current_pos[0]] = True
+
+        if current_pos[1] != prev_pos[1]:
+            posible_moves[:, current_pos[1]] = True
+
+        return list(zip(*np.nonzero(posible_moves * bool_table)))
+
+    res = [start_pos]
+    bool_table = x != 0
+
+    while True:
+        current_pos = res[-1]
+
+        bool_table[current_pos[0]][current_pos[1]] = False
+
+        if len(res) > 3:
+            bool_table[start_pos[0]][start_pos[1]] = True
+
+        posible_moves = get_posible_moves(bool_table, res)
+
+        if start_pos in posible_moves:
+            res.append(start_pos)
+            return res
+
+        if not posible_moves:
+            for i, j in res[1:-1]:
+                bool_table[i][j] = True
+
+            res = [start_pos]
+            continue
+
+        res.append(posible_moves[0])
+
+
+@log('Величина пересчета: {result}')
+def recalculate_plan(x: np.ndarray, cycle_path: List[Tuple[int, int]]) -> int:
+    """Пересчитать план. Возвращает величину пересчета."""
+    o = min([x[i][j] for i, j in cycle_path[1:-1:2]])
+
+    for k, (i, j) in enumerate(cycle_path[:-1]):
+        if k % 2 == 0:
+            x[i][j] += o
+        else:
+            x[i][j] -= o
+
+    return o
+
+
+def solve_transportation_problem(data: TransportationProblemData, use_nw_corner_method: bool = False):
+    logger.info(f'Дано:\na: {data.a}\nb: {data.b}\nc:\n{data.c}\nr:{data.r}')
+
+    diff = data.get_supply_demand_difference()
+
+    if diff < 0:
+        data.add_dummy_supplier(-diff)
+    elif diff > 0:
+        data.add_dummy_customer(diff)
+
+    if use_nw_corner_method:
+        x = get_start_plan_by_north_west_corner_method(data)
+    else:
+        x = get_start_plan_by_min_element_method(data)
+
+    while True:
+        if is_degenerate_plan(x):
+            return
+
+        data.calculate_cost(x)
+
+        p = data.calculate_potentials(x)
+
+        if data.is_plan_optimal(x, p):
+            return x
+
+        cycle_path = find_cycle_path(x, data.get_best_free_cell(x, p))
+        recalculate_plan(x, cycle_path)
+        logger.info(f'\nПлан после пересчета:\n{x}')
+
+
+if __name__ == '__main__':
+    data = TransportationProblemData(
+        a=[4, 6, 8],
+        b=[3, 6, 5, 7],
+        c=[
+            [2, 4, 1, 3],
+            [4, 8, 2, 4],
+            [2, 2, 6, 5],
+        ],
+    )
+
+    solve_transportation_problem(data, True)
