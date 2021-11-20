92a7ab21a27385c144b43bc6412530be35aa82f3
electrobullet <dmitry.pigasin@yandex.ru>
2021-11-20 20:11:48 +0300

    Update Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:app
+web: gunicorn run_server:app
--- example.py
+++ example.py
@@ -4,15 +4,16 @@ import numpy as np
 
 import transportation_problem as tp
 
-data = tp.Data(
-    np.array([12, 30, 13]),
-    np.array([23, 40, 12, 32]),
-    np.array([
-        [64, 32, 45, 12],
-        [32, 78, 23, 90],
-        [88, 67, 10, 32],
-    ]),
-)
+if __name__ == '__main__':
+    data = tp.Data(
+        np.array([12, 30, 13]),
+        np.array([23, 40, 12, 32]),
+        np.array([
+            [64, 32, 45, 12],
+            [32, 78, 23, 90],
+            [88, 67, 10, 32],
+        ]),
+    )
 
-tp.report.save_html_to_file(tp.solve(data), 'report.html')
-os.startfile('report.html')
+    tp.report.save_html_to_file(tp.solve(data), 'report.html')
+    os.startfile('report.html')
